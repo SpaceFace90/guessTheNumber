db473b5e2d187082524c477cbe0bd6991e951cce
SpaceFace90 <space-face@web.de>
2026-06-09 11:16:26 +0200

    added game

---
+++ Game.java
@@ -0,0 +1,12 @@
+public class Game {
+
+    private String spieler;
+    private int spielerVersuche;
+    private int guess;
+    private int zielZahl;
+    private int maxVersuche;
+    private int range;
+    private String hint;
+
+    
+}
